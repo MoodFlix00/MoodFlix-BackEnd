10ecd8ca28668fcef83a0b53126f0e88a276f3bd
준영킴 <zvpdrnls@gmail.com>
2025-07-21 23:27:47 +0900

    feat: review 엔티티 생성

---
+++ src/main/java/com/duck/moodflix/users/domain/entity/Review.java
@@ -0,0 +1,36 @@
+package com.duck.moodflix.users.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Float rating;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
