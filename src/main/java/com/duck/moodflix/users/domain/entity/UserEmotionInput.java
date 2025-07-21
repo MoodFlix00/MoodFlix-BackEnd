dd1cfe6859f4e3252444a8bdf5ea65ef5da195b6
준영킴 <zvpdrnls@gmail.com>
2025-07-21 23:28:39 +0900

    feat: useremotioninput 엔티티 생성

---
+++ src/main/java/com/duck/moodflix/users/domain/entity/UserEmotionInput.java
@@ -0,0 +1,31 @@
+package com.duck.moodflix.users.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_emotion_inputs")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEmotionInput {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String inputText;
+
+    private LocalDateTime createdAt;
+}
+
