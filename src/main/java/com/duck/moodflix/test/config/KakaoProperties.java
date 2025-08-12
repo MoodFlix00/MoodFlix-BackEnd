ac3e336eefae82067f48ae2334e727d8e8cdf8db
준영킴 <zvpdrnls@gmail.com>
2025-08-12 11:51:21 +0900

    feat: 카카오 로그인을 위한 설정 Properties 추가

---
+++ src/main/java/com/duck/moodflix/test/config/KakaoProperties.java
@@ -0,0 +1,16 @@
+package com.duck.moodflix.test.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "kakao")
+public class KakaoProperties {
+    private String clientId;
+    private String redirectUri;
+
+}
