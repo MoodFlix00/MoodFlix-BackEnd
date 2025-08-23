30594650d9a494e7a650b2180cf27cc06186b771
준영킴 <zvpdrnls@gmail.com>
2025-08-23 15:50:50 +0900

    feat: Swagger UI 연동을 통한 API 문서 자동화

---
+++ src/main/java/com/duck/moodflix/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.duck.moodflix.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        // API 문서의 기본 정보 설정
+        Info info = new Info()
+                .title("MoodFlix API Document")
+                .version("v1.0.0")
+                .description("MoodFlix 프로젝트의 API 명세서입니다.");
+
+        // JWT 인증 스키마 설정
+        String jwtSchemeName = "Bearer Authentication";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
