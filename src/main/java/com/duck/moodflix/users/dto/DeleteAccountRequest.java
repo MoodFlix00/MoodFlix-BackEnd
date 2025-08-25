25821670f26c3d1b2ada038bcd439bda969954bd
준영킴 <zvpdrnls@gmail.com>
2025-08-25 13:54:47 +0900

    fix: DeleteAccountRequest DTO 보안 및 명세 개선

--- src/main/java/com/duck/moodflix/users/dto/DeleteAccountRequest.java
+++ src/main/java/com/duck/moodflix/users/dto/DeleteAccountRequest.java
@@ -1,5 +1,6 @@
 package com.duck.moodflix.users.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,12 @@ import lombok.Setter;
 @Schema(description = "회원 탈퇴 요청 DTO")
 public class DeleteAccountRequest {
 
-    // [수정] @NotBlank 제거. 소셜 계정은 비밀번호가 없을 수 있으므로.
-    @Schema(description = "계정 확인을 위한 현재 비밀번호 (자체 로그인 사용자만 해당)", example = "password123")
+    // [수정] Schema 및 JsonProperty 어노테이션 추가
+    @Schema(
+            description = "계정 확인을 위한 현재 비밀번호 (자체 로그인 사용자만 해당)",
+            example = "password123",
+            requiredMode = Schema.RequiredMode.NOT_REQUIRED // Swagger 문서에 선택값으로 표시
+    )
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // 요청 시에만 사용하고, 응답에는 포함하지 않음
     private String password;
 }
