c7507104f073cb9d1dfa8faa3e1d09a904a998d0
준영킴 <zvpdrnls@gmail.com>
2025-08-28 18:41:07 +0900

    feat: CI/CD를 위한 H2 인메모리 데이터베이스 설정

--- src/main/java/com/duck/moodflix/MoodFlixApplication.java
+++ src/main/java/com/duck/moodflix/MoodFlixApplication.java
@@ -2,7 +2,11 @@ package com.duck.moodflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test") //
 @SpringBootApplication
 public class MoodFlixApplication {
 
