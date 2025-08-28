85751d3a79b02ff1ce392689d9e8e18290d88f1f
준영킴 <zvpdrnls@gmail.com>
2025-08-28 18:44:07 +0900

    feat: test 클래스에 어노테이션 추가

--- src/main/java/com/duck/moodflix/MoodFlixApplication.java
+++ src/main/java/com/duck/moodflix/MoodFlixApplication.java
@@ -2,11 +2,7 @@ package com.duck.moodflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test") //
 @SpringBootApplication
 public class MoodFlixApplication {
 
--- src/test/java/com/duck/moodflix/MoodFlixApplicationTests.java
+++ src/test/java/com/duck/moodflix/MoodFlixApplicationTests.java
@@ -2,7 +2,9 @@ package com.duck.moodflix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MoodFlixApplicationTests {
 
